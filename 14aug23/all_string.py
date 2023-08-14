1b5ed397b965ab3f26e9a5bc5180dc80d3a1443f
AnanyaKarne <ananyakarne02@gmail.com>
2023-08-14 23:00:17 +0530

    converttostring

--- 14aug23/all_string.py
+++ 14aug23/all_string.py
@@ -1,2 +1,2 @@
 a = input('number: ') # this input will be converted to the string
-print(int(a)-4) #so we'll use *int* function to convert
+print(int(a)-4) #so we'll use *int* function to convert. You can also use *float()
