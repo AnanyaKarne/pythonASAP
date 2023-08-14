f0c889ae1e538c90776844ccef0bf4565e656897
AnanyaKarne <ananyakarne02@gmail.com>
2023-08-15 00:30:32 +0530

    conditionoperators

---
+++ 14aug23/conditionn.py
@@ -0,0 +1,9 @@
+# a = 'hello'
+# b = 'HeLlo'
+print('a'=='b')
+print('a'!='b')
+print('a'>'b')
+print('a'<'b')
+print("a is : ", ord('a'))
+print("b is : ", ord('b')) 
+#ord('x') OR ord(), shows the ordinal value of the character
