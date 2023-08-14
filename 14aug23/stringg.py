fa951c15dc220f95f0a100c47e35f943e940021d
AnanyaKarne <ananyakarne02@gmail.com>
2023-08-15 00:18:10 +0530

    stringmethods

---
+++ 14aug23/stringg.py
@@ -0,0 +1,9 @@
+hello = "hello"
+print(type(hello))
+#formatting the text we gonna use methods on string
+print(hello.upper())
+print(hello.lower())
+print(hello.capitalize())
+print(hello.count('l'))# you also need to make sure if you are searching for lowecase or capitals
+print(hello.count('L'))
+print(hello.upper().count('L'))
