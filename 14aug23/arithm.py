18af281ec8b61190a6490137d5b6a8e0e08da396
AnanyaKarne <ananyakarne02@gmail.com>
2023-08-14 22:46:07 +0530

    Arithmetic

---
+++ 14aug23/arithm.py
@@ -0,0 +1,29 @@
+print("adding:")
+a = 2
+b = 4
+ans = a + b
+print(ans)
+print("multiply: ")
+x = 3
+y = 2.5
+ans = x * y
+print(ans)
+print("Division: ")
+p= 9
+q= 3
+ans = p/q
+ans1 = int(p/q)
+print(ans)
+print(ans1)
+print("Exponents: ")
+a = 3
+b = 2
+print(a ** b) #raises a to the power b
+print("Floor Division: ")
+a = 10
+b = 3
+print(a//b)
+print("Mod: ")
+a = 10
+b = 3
+print(a%b)
