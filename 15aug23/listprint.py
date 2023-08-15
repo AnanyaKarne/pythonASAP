e2abbe50bbec66a462c79dc87ca7f7419aeae5c4
AnanyaKarne <ananyakarne02@gmail.com>
2023-08-15 20:41:53 +0530

    for loop and range()

---
+++ 15aug23/listprint.py
@@ -0,0 +1,10 @@
+#printing list
+for i in [23,42,543,'gfds']:
+    print(i)
+
+x = [34,5342,1234,635,125,142]
+for i in range(len(x)):
+    print(x[i])
+for i, element in enumerate(x):
+    print(i, element)
+
--- 15aug23/loops.py
+++ 15aug23/loops.py
@@ -1,11 +1,20 @@
 for i in range(10):
     print(i, end= " ")
+
 print()
+
 for i in range(1, 10):
     print(i, end= " ")
+
 print()    
+
 for i in range(0, 10 , 2):
     print(i, end= " ")
+
 print()    
+
 for i in range(10, -1, -1):
     print(i, end= " ")
+
+print()    
+
