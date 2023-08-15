d9777d71fc90f2e4a0605b83800801377b6271ce
AnanyaKarne <ananyakarne02@gmail.com>
2023-08-16 00:51:16 +0530

    dictionary1

--- 15aug23/dict.py
+++ 15aug23/dict.py
@@ -1,2 +1,13 @@
 d = {'a' : 432, 'b': 565}
 print(d['b'])
+d[2] = 'dfs'
+d['re'] = [543,65,65,4]
+# print(d)
+# print('a' in  d)
+# print(d.values())
+# print(list(d.values()))
+for key, value in d.items():
+    print(key, value)
+
+for key in d:
+    print(key, d[key])
